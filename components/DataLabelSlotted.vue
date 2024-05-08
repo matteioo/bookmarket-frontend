4c974e83ad5367e68f4a0c625e491f2689b4559e
Matteo <26168940+matteioo@users.noreply.github.com>
2024-05-08 14:54:57 +0200

    Create DataLabel component with support for custom slot content.

---
+++ components/DataLabelSlotted.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full">
+    <span class="block text-sm text-gray-600 dark:text-gray-400">{{ props.label }}</span>
+    <span class="block text-gray-700 dark:text-gray-300">
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
