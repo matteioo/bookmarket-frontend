b5f3e44adf7fe8e21cb8488014a378f01bb840d0
Matteo <26168940+matteioo@users.noreply.github.com>
2024-06-30 21:11:06 +0200

    Finished with selling books. Implemented check wheter active stat of book changed, showing change of state in the frontend.

--- components/checkout/offer/item/Sell.vue
+++ components/checkout/offer/item/Sell.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="animate-flyIn flex flex-row group hover:bg-white dark:hover:bg-gray-900 rounded overflow-hidden">
-    <div class="flex-grow grid grid-cols-12 p-2 gap-x-2 gap-y-1">
-      <DataLabel label="Titel" :data="props.modelValue.book.title" class="col-span-7" />
-      <DataLabel label="Autoren" :data="props.modelValue.book.authors" class="col-span-5" />
-      <DataLabel label="ISBN" :data="props.modelValue.book.isbn" class="col-span-3" />
-      <DataLabel label="Auflage" :data="String(props.modelValue.book.edition)" class="col-span-7" />
-      <DataLabel label="Preis" :data="formatPrice(props.modelValue.price)" class="col-span-2" />
+  <div class="animate-flyIn group rounded overflow-hidden" :class="{ 'hover:bg-white dark:hover:bg-gray-900': isActive, 'border border-orange-300 dark:border-orange-600 bg-orange-50/50 hover:bg-orange-50/25 dark:bg-orange-950/25 dark:hover:bg-orange-950/50': !isActive }">
+    <div class="flex flex-row">
+      <div class="flex-grow grid grid-cols-12 p-2 gap-x-2 gap-y-1">
+        <DataLabel label="Titel" :data="props.modelValue.book.title" class="col-span-7" />
+        <DataLabel label="Autoren" :data="props.modelValue.book.authors" class="col-span-5" />
+        <DataLabel label="ISBN" :data="props.modelValue.book.isbn" class="col-span-3" />
+        <DataLabel label="Auflage" :data="String(props.modelValue.book.edition)" class="col-span-2" />
+        <DataLabel label="Angebot-ID" :data="String(props.modelValue.id)" class="col-span-3" />
+        <DataLabel label="Ort" :data="props.modelValue.location" class="col-span-2" />
+        <DataLabel label="Preis" :data="formatPrice(props.modelValue.price)" class="col-span-2" />
+      </div>
+      <UButton
+        icon="i-heroicons-trash"
+        size="sm"
+        color="red"
+        variant="ghost"
+        class="flex-grow-0 rounded-none"
+        @click="deleteItem"
+      />
     </div>
-    <UButton
-      icon="i-heroicons-trash"
-      size="sm"
-      color="red"
-      variant="ghost"
-      class="flex-grow-0 rounded-none"
-      @click="deleteItem"
-    />
+    <div v-if="!isActive" class="p-2 flex flex-row items-center gap-x-2 bg-orange-100 dark:bg-orange-900 text-orange-800 dark:text-orange-100">
+      <UIcon name="i-heroicons-exclamation-triangle" class="flex-shrink-0 w-5 h-5" />
+      <span class="text-sm">Dieses Buch ist nicht mehr verfügbar!</span>
+    </div>
   </div>
 </template>
 
@@ -30,6 +38,8 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['delete-item']);
+
+const isActive = computed(() => props.modelValue.active);
 
 const deleteItem = () => {
   emit('delete-item', props.modelValue);
--- pages/fv/sell/index.vue
+++ pages/fv/sell/index.vue
@@ -9,16 +9,13 @@
           <div>
             <UButtonGroup orientation="horizontal" class="w-full">
               <UInput v-model="state.offerId" name="offerId" placeholder="1234" step="1" min="0" class="flex-grow" @keydown.enter="searchOffer" />
-              <UButton type="submit" icon="i-heroicons-plus" color="gray" :disabled="isOfferIdValid?.error" :loading="loadingOffer" @click="searchOffer"/>
+              <UButton type="submit" icon="i-heroicons-plus" color="gray" :disabled="errorMsg != null" :loading="loadingOffer" @click="searchOffer"/>
             </UButtonGroup>
-            <div v-if="isOfferIdValid?.error" class="text-red-500 dark:text-red-400 text-sm mt-1">
-              {{ isOfferIdValid.message }}
-            </div>
-            <div v-else-if="selectedOffer && !selectedOffer.active" class="text-red-500 dark:text-red-400 text-sm mt-1">
-              Angebot mit der ID {{ selectedOffer.id }} ist nicht aktiv...
+            <div v-if="isIdInvalid" class="text-red-500 dark:text-red-400 text-sm mt-1">
+              {{ errorMsg }}
             </div>
           </div>
-          <div v-if="selectedOffer && selectedOffer.active" class="w-full mt-4">
+          <div v-if="selectedOffer && selectedOffer.active && !isIdInvalid" class="w-full mt-4">
             <div class="grid grid-cols-6 gap-2">
               <DataLabel label="Titel" :data="selectedOffer.book.title" class="col-span-6" />
               <DataLabel label="ISBN" :data="selectedOffer.book.isbn" class="col-span-3" />
@@ -39,7 +36,7 @@
                 color="primary"
                 variant="outline"
                 label="Hinzufügen"
-                :disabled="!selectedOffer"
+                :disabled="!selectedOffer || isIdInvalid"
                 @click="addOffer"
               />
             </div>
@@ -52,19 +49,32 @@
           <div v-for="(offer, index) in addedOffers" :key="offer.id">
             <CheckoutOfferItemSell v-model="addedOffers[index]" @delete-item="removeOffer" />
           </div>
-          <div class="w-full inline-flex justify-end items-center gap-x-2">
-            <div>
-              Gesamtpreis: <b>{{ selectedOfferPrice }}</b>
-            </div>
+          <div class="w-full inline-flex justify-between items-center">
             <div>
               <UButton
                 size="sm"
-                color="primary"
-                variant="solid"
-                label="Verkaufen"
-                :disabled="addedOffers.length === 0"
-                @click="confirmModalOpen = true"
+                color="white"
+                icon="i-heroicons-arrow-path"
+                variant="ghost"
+                label="Neu laden"
+                :loading="updatingAddedOffers"
+                @click="updateOffers"
               />
+            </div>
+            <div class="inline-flex items-center gap-x-2">
+              <div>
+                Gesamtpreis: <b>{{ selectedOfferPrice }}</b>
+              </div>
+              <div>
+                <UButton
+                  size="sm"
+                  color="primary"
+                  variant="solid"
+                  label="Verkaufen"
+                  :disabled="addedOffers.length === 0 || !addedOffersActive || updatingAddedOffers"
+                  @click="confirmModalOpen = true"
+                />
+              </div>
             </div>
           </div>
           <UModal v-model="confirmModalOpen">
@@ -82,6 +92,7 @@
                     color="primary"
                     variant="outline"
                     label="Abbrechen"
+                    :disabled="loadingCheckout"
                     @click="confirmModalOpen = false"
                   />
                   <UButton
@@ -89,7 +100,8 @@
                     color="primary"
                     variant="solid"
                     label="Bestätigen"
-                    @click="confirmModalOpen = false"
+                    :loading="loadingCheckout"
+                    @click="checkout"
                   />
                 </div>
               </template>
@@ -120,13 +132,16 @@ const loadingOffer = ref(false)
 const selectedOffer = ref<Offer | null>(null)
 const addedOffers = ref<Offer[]>([])
 const confirmModalOpen = ref(false)
+const errorMsg = ref<string | null>(null)
+const loadingCheckout = ref(false)
+const updatingAddedOffers = ref(false)
 
 const state = reactive({
   offerId: '',
 })
 
+const isIdInvalid = computed(() => errorMsg.value !== null)
 const selectedOfferPrice = computed(() => formatPrice(addedOffers.value.reduce((acc, offer) => acc + offer.price, 0)))
-
 const offerCount = computed(() => {
   if (addedOffers.value.length === 1) {
     return 'ein Buch';
@@ -134,21 +149,24 @@ const offerCount = computed(() => {
     return `${addedOffers.value.length} Bücher`;
   }
 })
+const addedOffersActive = computed(() => addedOffers.value.every(offer => offer.active))
 
-const isOfferIdValid = computed(() => {
-  if (state.offerId === '') return null;
-  if (Number.isInteger(Number(state.offerId)) && Number(state.offerId) >= 0) {
-    if (addedOffers.value.find(offer => offer.id === Number(state.offerId))) {
-      return { error: true, message: 'Angebot mit dieser ID wurde bereits hinzugefügt.' };
-    } else {
-      return { error: false, message: null };
-    }
+function isOfferIdValid() {
+  if (!Number.isInteger(Number(state.offerId)) || Number(state.offerId) < 0) {
+    errorMsg.value = 'Angebots-ID muss eine positive Zahl sein!';
+  } else if (addedOffers.value.find(offer => offer.id === Number(state.offerId))) {
+    errorMsg.value = 'Angebot mit dieser ID wurde bereits hinzugefügt.';
+  } else {
+    errorMsg.value = null;
   }
-  return { error: true, message: 'Angebots-ID muss eine positive Zahl sein!' };
-})
+}
 
 async function searchOffer () {
-  if (!isOfferIdValid.value) return;
+  isOfferIdValid();
+  if (errorMsg.value) {
+    selectedOffer.value = null;
+    return;
+  };
   loadingOffer.value = true;
 
   const offerId = Number(state.offerId);
@@ -164,16 +182,17 @@ async function searchOffer () {
       selectedOffer.value = offerById;
 
       if (!selectedOffer.value.active) {
+        errorMsg.value = 'Angebot ist nicht aktiv!';
         state.offerId = '';
+        selectedOffer.value = null;
       }
     } else {
       selectedOffer.value = null;
       console.error('No offer found with ID:', offerId);
     }
-
-  } catch (error) {
+  } catch (error: any) {
     selectedOffer.value = null;
-    console.error('Failed to fetch offer:', error);
+    console.error('Failed to fetch offer:', error.data);
   }
 
   loadingOffer.value = false;
@@ -194,5 +213,104 @@ function removeOffer(offer: Offer) {
     addedOffers.value.splice(index, 1);
   }
   console.log('removed offer - offers:', addedOffers.value);
+}
+
+async function checkout() {
+  loadingCheckout.value = true;
+  console.log('checkout', addedOffers.value, addedOfferIds());
+
+  try {
+    await $fetch(useRuntimeConfig().public.apiUrl + '/offers/sell' , {
+      method: 'POST',
+      headers: {
+        Authorization: `${token.value}`,
+      },
+      body: JSON.stringify({
+        offers: addedOfferIds(),
+      }),
+    });
+
+    useToast().add({
+      title: 'Bücher wurden erfolgreich verkauft!',
+      icon: 'i-heroicons-check-circle',
+      color: 'green',
+    })
+
+    await navigateTo({
+      path: '/fv',
+    })
+
+  } catch (error: any) {
+    console.error('Failed to fetch offer:', error.data.error);
+    confirmModalOpen.value = false;
+    await updateOffers();
+    useToast().add({
+      title: 'Fehler',
+      description: 'Bücher konnten nicht verkauft werden!',
+      icon: 'i-heroicons-exclamation-triangle',
+      color: 'red',
+      actions: [{
+        label: 'Zurücksetzen',
+        variant: 'outline',
+        color: 'red',
+        click: () => {
+          addedOffers.value = [];
+        },
+      }]
+    })
+  } finally {
+    loadingCheckout.value = false;
+  }
+
+  confirmModalOpen.value = false;
+}
+
+function addedOfferIds() {
+  return addedOffers.value.map(offer => offer.id);
+}
+
+async function updateOffers() {
+  updatingAddedOffers.value = true;
+  // Filter all offers where offer.id is null
+  addedOffers.value = addedOffers.value.filter(offer => offer.id);
+
+  // Create an array of promises to refetch all offers in parallel
+  const fetchPromises = addedOffers.value.map(async (offer) => {
+    if (!offer.id) {
+      return;
+    }
+
+    const refetchedOffer = await refetchOffer(offer.id);
+    if (refetchedOffer) {
+      offer.active = refetchedOffer.active;
+    } else {
+      // If the offer is not found, set active to false
+      offer.active = false;
+    }
+  });
+
+  // Wait for all fetch promises to complete
+  await Promise.all(fetchPromises);
+  updatingAddedOffers.value = false;
+}
+
+async function refetchOffer(id: number): Promise<Offer | null> {
+  try {
+    const offerById = await $fetch<Offer>(useRuntimeConfig().public.apiUrl + '/offers/' + id, {
+      method: 'GET',
+      headers: {
+        Authorization: `${token.value}`,
+      },
+    });
+
+    if (offerById) {
+      return offerById;
+    } else {
+      console.error('No offer found with ID:', id);
+      return null;
+    }
+  } catch (error: any) {
+    return null;
+  }
 }
 </script>
