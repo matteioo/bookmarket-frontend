5a3666a1b738f89996f2158018e042dcc1fc1abe
Matteo <26168940+matteioo@users.noreply.github.com>
2025-02-01 02:58:57 +0100

    Add first amount of seo tags for the public search page.

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,17 @@
 </template>
 
 <script setup lang="ts">
+useSeoMeta({
+  ogTitle: 'Bückerbörse der FVJus',
+  twitterTitle: 'Bückerbörse der FVJus',
+  description: 'Verkaufe deine Bücher, bevor sie zu Staubfängern werden und schlage bei gebrauchten Büchern von anderen Studierenden zu. Die Bücherbörse der FVJus macht es möglich - ganz ohne Provision und Gebühren.',
+  ogDescription: 'Verkaufe deine Bücher, bevor sie zu Staubfängern werden und schlage bei gebrauchten Büchern von anderen Studierenden zu. Die Bücherbörse der FVJus macht es möglich - ganz ohne Provision und Gebühren.',
+  twitterDescription: 'Verkaufe deine Bücher, bevor sie zu Staubfängern werden und schlage bei gebrauchten Büchern von anderen Studierenden zu. Die Bücherbörse der FVJus macht es möglich - ganz ohne Provision und Gebühren.',
+  ogImage: '',
+  twitterCard: 'summary',
+  themeColor: '#5c2849',
+})
+
 definePageMeta({
   auth: false
 })
