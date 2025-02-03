4cea62d9e304f8032f9218e52ab576a06c8526da
Matteo <26168940+matteioo@users.noreply.github.com>
2025-02-03 20:38:47 +0100

    add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,17 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+    }
+  },
   dayjs: {
     locales: ['de'],
     plugins: ['relativeTime', 'utc', 'timezone'],
