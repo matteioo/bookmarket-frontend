8ae3252fae78c872bba81e46e8ef15e9ab4514a7
Matteo <26168940+matteioo@users.noreply.github.com>
2025-02-03 13:47:11 +0100

    Modernize navbar and dashboard route text.

--- layouts/protected.vue
+++ layouts/protected.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="min-h-dvh container mx-auto flex flex-col items-center px-4">
     <header class="flex-grow-0 w-full flex justify-between py-2">
-      <NuxtLink to="/fv" class="text-primary-900 dark:text-primary-100 text-xl flex items-center gap-x-2"><UIcon name="i-heroicons-scale" /><span>FV Jus</span></NuxtLink>
+      <NuxtLink to="/fv" class="text-primary-900 dark:text-primary-100 text-xl flex items-center gap-x-2"><UIcon name="i-heroicons-circle-stack" /><span>Bücherbörse @ FVJus</span></NuxtLink>
       <div class="font-medium">
-        <UButton color="gray" variant="ghost" size="lg" to="/fv/offers"><span>Angebote</span></UButton>
-        <UButton color="gray" variant="ghost" size="lg" to="/fv/sellers"><span>Verkäufer:innen</span></UButton>
-        <UButton color="gray" variant="ghost" size="lg" to="/fv/search"><span>Suchen</span></UButton>
-        <UButton color="gray" variant="ghost" size="lg" to="/fv/sell"><span>Verkaufen</span></UButton>
-        <UButton color="gray" variant="ghost" size="lg" icon="i-heroicons-arrow-right-start-on-rectangle" trailing @click="logoutAndRedirect">Logout</UButton>
+        <UHorizontalNavigation :links="links" />
       </div>
     </header>
     <div class="flex-grow w-full flex flex-col m-2">
@@ -20,12 +16,33 @@
 </template>
 
 <script setup lang="ts">
-const { signOut } = useAuth();
-const router = useRouter();
+const { signOut } = useAuth()
+const router = useRouter()
 
 const logoutAndRedirect = async () => {
   // TODO: Error fetching signOut endpoint (default /logout because not set), even though we simply want to remove the token as logout action
   await signOut({ callbackUrl: '/login' })
-  router.push('/login');
+  router.push('/login')
 };
+
+const links = [{
+  label: 'Suchen',
+  to: '/fv/search',
+  icon: 'i-heroicons-magnifying-glass',
+}, {
+  label: 'Verkaufen',
+  to: '/fv/sell',
+  icon: 'i-heroicons-shopping-cart',
+}, {
+  label: 'Angebote',
+  to: '/fv/offers',
+  icon: 'i-heroicons-tag',
+}, {
+  label: 'Verkäufer:innen',
+  to: '/fv/sellers',
+  icon: 'i-heroicons-user-group',
+}, {
+  label: 'Logout',
+  click: logoutAndRedirect
+}]
 </script>
