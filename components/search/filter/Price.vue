f85c421c1eaa0d0a8b8a6f8e947c1674ee1ee92d
Matteo <26168940+matteioo@users.noreply.github.com>
2025-02-07 19:37:57 +0100

    Format currency input correctly.

--- components/search/filter/Price.vue
+++ components/search/filter/Price.vue
@@ -10,15 +10,15 @@
       />
 
       <template #panel>
-        <UForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit">
+        <UForm :state="state" class="space-y-4" @submit="onSubmit">
           <div class="p-4 flex flex-col gap-y-4">
             <div class="inline-flex flex-row gap-x-2 items-center">
               <UFormGroup class="w-24" name="minPrice">
-                <UInput v-model="state.min" placeholder="Min." type="number" step="0.01" min="0" :max="state.max ?? 999.99" />
+                <FormInputPrice v-model="state.min" label="maxPrice" size="md" placeholder="Min." :min-price="0" :max-price="state.max ?? 999.99" />
               </UFormGroup>
               <span>bis</span>
               <UFormGroup class="w-24" name="maxPrice">
-                <UInput v-model="state.max" placeholder="Max." type="number" step="0.01" :min="state.min ?? 0" max="999.99" />
+                <FormInputPrice v-model="state.max" label="maxPrice" size="md" placeholder="Max." :min-price="state.min ?? 0" :max-price="999.99" />
               </UFormGroup>
             </div>
             <div class="inline-flex flex-row-reverse gap-x-2 justify-stretch flex-wrap">
@@ -47,26 +47,27 @@
 </template>
 
 <script setup lang="ts">
-import type { FormError, FormSubmitEvent } from '#ui/types'
+import type { FormSubmitEvent } from '#ui/types'
 import type { Filter, PriceFilter } from '~/interfaces/SearchFilters'
 
 const props = defineProps({
   priceFilter: {
     type: Object as PropType<{
-      active: boolean;
+      active: boolean
       value: {
-        min: number | undefined;
-        max: number | undefined;
-      };
+        min: number | undefined
+        max: number | undefined
+      }
     }>,
     required: true,
   }
-});
-const emit = defineEmits(['update:priceFilter']);
+})
+const emit = defineEmits(['update:priceFilter'])
 
-const popoverOpen = ref(false);
-const localPriceFilter = ref<Filter<PriceFilter>>(props.priceFilter);
-const filterActive = computed(() => props.priceFilter.value.min !== undefined || props.priceFilter.value.max !== undefined);
+const popoverOpen = ref(false)
+const localPriceFilter = ref<Filter<PriceFilter>>(props.priceFilter)
+const filterActive = computed(() => props.priceFilter.value.min !== undefined || props.priceFilter.value.max !== undefined)
+const errors = ref<string[]>([])
 
 const state = reactive<PriceFilter>({
   min: localPriceFilter.value.value.min,
@@ -77,51 +78,53 @@ const state = reactive<PriceFilter>({
 watch(
   () => props.priceFilter,
   (newFilter) => {
-    state.min = newFilter.value.min;
-    state.max = newFilter.value.max;
-    localPriceFilter.value = newFilter;
+    state.min = newFilter.value.min
+    state.max = newFilter.value.max
+    localPriceFilter.value = newFilter
   },
   { immediate: true, deep: true }
 )
 
-const validate = (state: PriceFilter): FormError[] => {
-  const errors = []
-  if (state.min && state.min < 0) errors.push({ path: 'minPrice', message: 'Preis muss mind. 0 sein!' })
-  if (state.max && state.max > 999.99) errors.push({ path: 'maxPrice', message: 'Preis darf max. 999,99 sein!' })
-  if (state.max && state.max && state.max > state.max) errors.push({ path: 'maxPrice', message: 'Max. Preis muss größer als Min. Preis sein!' })
-  return errors
+const validate = (state: PriceFilter) => {
+  if (state.min && state.min < 0) errors.value.push('Min. Preis muss mindestens 0 € sein!')
+  if (state.max && state.max > 999.99) errors.value.push('Max. Preis muss kleiner 1.000 € sein!')
+  if (state.max && state.max && state.max > state.max) errors.value.push('Max. Preis muss größer als Min. Preis sein!')
 }
 
 const priceLabel = computed(() => {
   if (localPriceFilter.value.value.min !== undefined && localPriceFilter.value.value.max !== undefined) {
-    return `Preis: ${localPriceFilter.value.value.min.toFixed(2)} - ${localPriceFilter.value.value.max.toFixed(2)} €`;
+    return `Preis: ${localPriceFilter.value.value.min.toFixed(2)} - ${localPriceFilter.value.value.max.toFixed(2)} €`
   } else if (localPriceFilter.value.value.min !== undefined) {
-    return `Preis: ab ${localPriceFilter.value.value.min.toFixed(2)} €`;
+    return `Preis: ab ${localPriceFilter.value.value.min.toFixed(2)} €`
   } else if (localPriceFilter.value.value.max !== undefined) {
-    return `Preis: bis ${localPriceFilter.value.value.max.toFixed(2)} €`;
+    return `Preis: bis ${localPriceFilter.value.value.max.toFixed(2)} €`
   } else {
-    return 'Preis';
+    return 'Preis'
   }
 })
 
 async function onSubmit (event: FormSubmitEvent<PriceFilter>) {
+  validate(event.data)
+
   if (!event.data.min && !event.data.max) {
-    resetModal();
+    resetModal()
+  } else if (errors.value.length) {
+    return
   } else {
-    localPriceFilter.value = { active: true, value: { min: event.data.min, max: event.data.max } };
-    emit('update:priceFilter', localPriceFilter.value);
+    localPriceFilter.value = { active: true, value: { min: event.data.min, max: event.data.max } }
+    emit('update:priceFilter', localPriceFilter.value)
   }
 
-  popoverOpen.value = false;
+  popoverOpen.value = false
 }
 
 function resetModal () {
-  state.min = undefined;
-  state.max = undefined;
-  localPriceFilter.value = { active: false, value: { min: undefined, max: undefined } };
+  state.min = undefined
+  state.max = undefined
+  localPriceFilter.value = { active: false, value: { min: undefined, max: undefined } }
 
-  emit('update:priceFilter', localPriceFilter.value);
+  emit('update:priceFilter', localPriceFilter.value)
 
-  popoverOpen.value = false;
+  popoverOpen.value = false
 }
 </script>
