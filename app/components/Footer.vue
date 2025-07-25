7f5599234162380ae47eab89c16a182343012399
Matteo <26168940+matteioo@users.noreply.github.com>
2025-07-25 23:46:45 +0200

    Change footer text color for better contrast ratio in light mode.

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <footer class="inline-flex gap-x-2 gap-y-1 justify-center flex-wrap my-4 text-neutral-400">
+  <footer class="inline-flex gap-x-2 gap-y-1 justify-center flex-wrap my-4 text-neutral-500 dark:text-neutral-400">
     <span>&copy; 2024 - Bücherbörse der FV Jus</span>
     <NuxtLink class="shrink-0" target="_blank" :to="`https://github.com/matteioo/bookmarket-frontend/releases/tag/${useRuntimeConfig().public.appVersion}`">
       <UBadge color="primary" variant="subtle">{{ useRuntimeConfig().public.appVersion }}</UBadge>
